a4dd0cfcd7d101fc4d4725ad63e0d58181fecb5d
MAIN\nac5961 <nac5961@g.rit.edu>
2017-10-26 10:51:13 -0400

    Started Camera Exercise

--- 17_ProjectionsAndCamera/AppClass.cpp
+++ 17_ProjectionsAndCamera/AppClass.cpp
@@ -53,15 +53,26 @@ void Application::Display(void)
 	//draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 
-	static float fPos = 0.0f;
-	m_pCamera->SetPosition(vector3(fPos, 0.0f, 10.0f));
-	m_pCamera->SetTarget(vector3(fPos, 0.0f, 9.0f));
-	fPos -= 0.01f;
+	//static float fPos = 0.0f;
+	//m_pCamera->SetPosition(vector3(fPos, 0.0f, 10.0f));
+	//m_pCamera->SetTarget(vector3(fPos, 0.0f, 9.0f));
+	//fPos -= 0.01f;
+
+	vector3 v3LookingAt = m_v3CameraPosition;
+	v3LookingAt.z -= 1.0f;
+	float fRatio = static_cast<float>(m_pSystem->GetWindowWidth()) / static_cast<float>(m_pSystem->GetWindowHeight());
+	//float fRatio = m_pSystem->GetWindowRatio();
+
+
+	//matrix4 m4Projection = glm::perspective(90.0f, fRatio, 30.01f, 1000.0f); //m_pCameraMngr->GetProjectionMatrix(); //determines how the camera looks at what it's looking at
+	matrix4 m4Projection = glm::ortho(-10.0f + v3LookingAt.x, 10.0f - v3LookingAt.x, -20.0f, 20.0f, 0.01f, 1000.0f);
+	matrix4 m4View = glm::lookAt(vector3(0, 0, 30) + m_v3CameraPosition, vector3(0, 0, 0) + v3LookingAt, AXIS_Y); //m_pCameraMngr->GetViewMatrix(); //where camera will be located and what it's looking at
+	matrix4 m4Model = ToMatrix4(m_qArcBall);
 
 	//draw the primitive
 	//m_pMesh->Render(m_pCamera->GetProjectionMatrix(), m_pCamera->GetViewMatrix(), ToMatrix4(m_qArcBall));
 	//m_pMesh->Render(m_pCamera, ToMatrix4(m_qArcBall));
-	m_pMesh2->Render(m_pCamera, glm::translate(vector3(0.0f, 0.0f, -5.0f)));
+	m_pMesh->Render(m4Projection, m4View, m4Model);
 
 	//render list call
 	m_uRenderCallCount = m_pMeshMngr->Render();
